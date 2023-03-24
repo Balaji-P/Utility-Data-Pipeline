fa0d3790df59c5baa1be99183c089b7b46a02f5a
O A <128834830+onyade@users.noreply.github.com>
2023-03-24 13:11:47 -0500

    Update start_platform.bash

--- start_platform.bash
+++ start_platform.bash
@@ -1,3 +1,8 @@
+# create a shell script to start the Microservices-based data processing platform.
+# call the ingest script to publish sensor data to the message broker
+# call the processor script to subscribe to the message broker
+# process the sensor data and publish the processed data to the database
+
 #!/bin/bash
 
 # Run the ingest script
